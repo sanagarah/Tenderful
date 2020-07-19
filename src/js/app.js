0b62a026407d8ab69adf6fc08b11048843f5a853
Sana Garah <sanambg98@gmail.com>
2020-07-19 21:55:56 +0300

    html + css files has been added

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
-App = {
-    web3Provider: null,
-    contracts: {},
+var name = document.querySelectorAll("#name").value;
+var age = document.querySelectorAll("#age").value;
+var info = document.querySelectorAll("#info").value;
 
-    init: async function () {
-        // Load pets.
-        $.getJSON('../pets.json', function (data) {
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function () {
+    // Load pets.
+    /** $.getJSON('../pets.json', function (data) {
             var petsRow = $('#petsRow');
             var petTemplate = $('#petTemplate');
 
@@ -18,38 +22,40 @@ App = {
 
                 petsRow.append(petTemplate.html());
             }
-        });
+        }); **/
 
-        return await App.initWeb3();
-    },
+    return await App.initWeb3();
+  },
 
-    initWeb3: async function () {
-// Modern dapp browsers...
-        if (window.ethereum) {
-            App.web3Provider = window.ethereum;
-            try {
-                // Request account access
-                await window.ethereum.enable();
-            } catch (error) {
-                // User denied account access...
-                console.error("User denied account access")
-            }
-        }
-// Legacy dapp browsers...
-        else if (window.web3) {
-            App.web3Provider = window.web3.currentProvider;
-        }
-// If no injected web3 instance is detected, fall back to Ganache
-        else {
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-        }
-        web3 = new Web3(App.web3Provider);
+  initWeb3: async function () {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access");
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
+    }
+    web3 = new Web3(App.web3Provider);
 
-        return App.initContract();
-    },
+    return App.initContract();
+  },
 
-    initContract: function () {
-        $.getJSON('Adoption.json', function (data) {
+  initContract: function () {
+    /**  $.getJSON('Adoption.json', function (data) {
             // Get the necessary contract artifact file and instantiate it with @truffle/contract
             var AdoptionArtifact = data;
             App.contracts.Adoption = TruffleContract(AdoptionArtifact);
@@ -59,97 +65,150 @@ App = {
 
             // Use our contract to retrieve and mark the adopted pets
             return App.markAdopted();
+        }); **/
+
+    return App.bindEvents();
+  },
+  //TODO Helloworld
+  bindEvents: function () {
+    $(document).on("click", ".btn-adopt", App.handleAdopt);
+    $(document).on("click", ".btn-register-nanny", App.registerNanny);
+  },
+
+  markAdopted: function (adopters, account) {
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed()
+      .then(function (instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.getAdopters.call();
+      })
+      .then(function (adopters) {
+        for (i = 0; i < adopters.length; i++) {
+          if (adopters[i] !== "0x0000000000000000000000000000000000000000") {
+            $(".panel-pet")
+              .eq(i)
+              .find("button")
+              .text("Success")
+              .attr("disabled", true);
+          }
+        }
+      })
+      .catch(function (err) {
+        console.log(err.message);
+      });
+  },
+
+  handleAdopt: function (event) {
+    event.preventDefault();
+
+    var petId = parseInt($(event.target).data("id"));
+
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed()
+        .then(function (instance) {
+          adoptionInstance = instance;
+
+          // Execute adopt as a transaction by sending account
+          return adoptionInstance.adopt(petId, { from: account });
+        })
+        .then(function (result) {
+          return App.markAdopted();
+        })
+        .catch(function (err) {
+          console.log(err.message);
         });
+    });
+  },
 
-        return App.bindEvents();
-    },
-//TODO Helloworld
-    bindEvents: function () {
-        $(document).on('click', '.btn-adopt', App.handleAdopt);
-        $(document).on('click', '.btn-register-nanny', App.registerNanny);
-    },
+  //Register Nanny
+  registerNanny: function (event) {
+    event.preventDefault();
+    window.location.href = "../NannyRegister.html";
+  },
 
-    markAdopted: function (adopters, account) {
-        var adoptionInstance;
+  registerButtonNanny: function (event) {
+    event.preventDefault();
 
-        App.contracts.Adoption.deployed().then(function (instance) {
-            adoptionInstance = instance;
+    var babySittersInstance;
 
-            return adoptionInstance.getAdopters.call();
-        }).then(function (adopters) {
-            for (i = 0; i < adopters.length; i++) {
-                if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-                    $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-                }
-            }
-        }).catch(function (err) {
-            console.log(err.message);
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.babySitters
+        .deployed()
+        .then(function (instance) {
+          babySittersInstance = instance;
+
+          // Execute adopt as a transaction by sending account
+          return babySittersInstance.registerAsNanny(name, age, info, {
+            from: account,
+          });
+        })
+        .then(function (result) {
+          return console.log("done");
+        })
+        .catch(function (err) {
+          console.log(err.message);
         });
-    },
+    });
+  },
 
-    handleAdopt: function (event) {
-        event.preventDefault();
+//Hire Nanny
+    hireNanny: function (event) {
+    event.preventDefault();
+    window.location.href = "../NannyRegister.html";
+  },
 
-        var petId = parseInt($(event.target).data('id'));
+  hireNanny: function (event) {
+    event.preventDefault();
 
-        var adoptionInstance;
+    var babySittersInstance;
 
-        web3.eth.getAccounts(function (error, accounts) {
-            if (error) {
-                console.log(error);
-            }
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
 
-            var account = accounts[0];
+      var account = accounts[0];
 
-            App.contracts.Adoption.deployed().then(function (instance) {
-                adoptionInstance = instance;
+      App.contracts.babySitters
+        .deployed()
+        .then(function (instance) {
+          babySittersInstance = instance;
 
-                // Execute adopt as a transaction by sending account
-                return adoptionInstance.adopt(petId, {from: account});
-            }).then(function (result) {
-                return App.markAdopted();
-            }).catch(function (err) {
-                console.log(err.message);
-            });
+          // Execute adopt as a transaction by sending account
+          return babySittersInstance.hireNanny( , {
+            from: account,
+          });
+        })
+        .then(function (result) {
+          return console.log("done");
+        })
+        .catch(function (err) {
+          console.log(err.message);
         });
-    },
+    });
+  },
 
-    registerNanny: function (event){
-        event.preventDefault();
-        window.location.href = "../nannyRegisteration.html";
-
-        
-    },
-
-    registerButtonNanny: function(event){
-        event.preventDefault();
-
-        var babySittersInstance;
-
-        web3.eth.getAccounts(function (error, accounts) {
-            if (error) {
-                console.log(error);
-            }
-
-            var account = accounts[0];
-
-            App.contracts.babySitters.deployed().then(function (instance) {
-                babySittersInstance = instance;
-
-                // Execute adopt as a transaction by sending account
-                return babySittersInstance.registerAsNanny( , , , {from: account});
-            }).then(function (result) {
-                return App.markAdopted();
-            }).catch(function (err) {
-                console.log(err.message);
-            });
-        });
-    }
 
 };
 
 $(function () {
-    $(window).load(function () {
-        App.init();
-    });
+  $(window).load(function () {
+    App.init();
+  });
 });
